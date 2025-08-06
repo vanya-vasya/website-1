536ef2fb560cf4c82cebcb375d360ad9d9e65754
vanya-vasya <vladimir.serushko@gmail.com>
2025-08-06 19:23:40 +0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,14 @@
-import {
-  clerkMiddleware,
-  createRouteMatcher
-} from '@clerk/nextjs/server';
+// middleware.ts
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher([
-  '/dashboard(.*)',
-]);
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
 });
 
+// важно: включаем Node.js runtime вместо Edge
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-  runtime: "nodejs"
+  runtime: 'nodejs',
 };
